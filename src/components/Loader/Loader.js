d61297cde4614c1355bcf1cfdd48b898c72c5fa9
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-08 22:20:06 +0200

    Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,9 @@
 // import React, { Component } from "react";
 // // import Loader from "react-loader-spinner";
 
-// export default class Loader extends Component {
+//class Loader extends Component {
 //   render() {
 //     return <Loader type="Bars" color="#00BFFF" height={80} width={80} />;
 //   }
 // }
+// export default
